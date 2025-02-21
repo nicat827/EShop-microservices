6d5edc36d2641297dfd9becaccf6c7cc6601bf6c
Nijat Majidov <97800703+nicat827@users.noreply.github.com>
2025-02-22 01:19:36 +0400

    minimal api endpoint using carter for create product, CreateProductHandler added, globalUsings created

---
+++ Services/Catalog/Catalog.API/Products/Create/CreateProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.Create
+{
+    internal record CreateProductRequest(string Name, IEnumerable<string> Categories,string Description,decimal Price);
+    internal record CreateProductResponse(Guid Id);
+    internal class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest req, ISender sender) =>
+            {
+                var command = req.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Products");
+             
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/Create/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using Shared.CQRS;
+
+namespace Catalog.API.Products.Create
+{
+    internal record CreateProductCommand(string Name, IEnumerable<string> Categories, string Description, decimal Price) 
+        : ICommand<CreateProductResult>;
+    internal record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
