14c05db077fc9eaf40e7901f7e8d57eb16e84c5e
Nicat Macidov <nicat.macidov@azintelecom.az>
2025-02-26 11:15:24 +0400

    delete product added

---
+++ Services/Catalog/Catalog.API/Products/Delete/DeleteProductEndpoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.Delete
+{
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var command = new DeleteProductCommand(id);
+                await sender.Send(command);
+                return Results.NoContent();
+            })
+                .WithName("DeleteProduct")
+                .Produces(StatusCodes.Status204NoContent)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithDescription("Delete product")
+                .WithSummary("Delete product");
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/Delete/DeleteProductHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Catalog.API.Products.Delete
+{
+    internal record DeleteProductCommand(Guid Id) : ICommand;
+    internal class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger)
+        : ICommandHandler<DeleteProductCommand>
+    {
+        public async Task<Unit> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductHandler called with command {@Command}", command);
+            Product product = await session.LoadAsync<Product>(command.Id, cancellationToken)
+                ?? throw new ProductNotFoundException();
+            session.Delete(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
