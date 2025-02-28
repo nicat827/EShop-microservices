61ae20d58c2657ef9b92e1610c20847801f243f3
Nicat Macidov <nicat.macidov@azintelecom.az>
2025-02-28 15:37:21 +0400

    global logging added

---
+++ Shared/Shared/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Shared.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> 
+        (ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest: notnull, IRequest<TResponse>
+        where TResponse : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("[START] Handle request={Request} - Response={Response}, ResponseContent={ResponseData}",
+                typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            var res = await next();
+
+            timer.Stop();
+            var elapsed = timer.Elapsed;
+            if (elapsed.Seconds >= 3)
+                logger.LogWarning("[SLOW] The request={Request} took {TimeTaken} seconds",
+                    typeof(TRequest).Name, elapsed.Seconds);
+
+            logger.LogInformation("[END] Handled request={Request}. Time: {Time}, Response={Response}",
+                typeof(TRequest).Name,elapsed.Milliseconds, typeof(TResponse).Name);
+
+            return res;
+        }
+    }
+}
--- Shared/Shared/Exceptions/Handler/CustomExceptionHandler.cs
+++ Shared/Shared/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Shared.Exceptions.Base;
@@ -14,6 +15,8 @@ namespace Shared.Exceptions.Handler
             int status = 500;
             if (exception is BaseException baseException)
                 status = (int)baseException.StatusCode;
+            if (exception is FluentValidation.ValidationException)
+                status = 400;
 
             httpContext.Response.StatusCode = status;
             await httpContext.Response.WriteAsJsonAsync(new {Status = status,Message = exception.Message},cancellationToken);
