8d99cf49bcf599fb7ad05a04ca06dd982df24d0f
Nijat Majidov <97800703+nicat827@users.noreply.github.com>
2025-03-15 19:10:38 +0400

    db created

---
+++ Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,11 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
+    {
+        public DbSet<Coupon> Coupons { get; set; }
+
+    }
+}
